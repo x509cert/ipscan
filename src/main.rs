bae223b4c9d162128c140d251a34d2e582ec38c9
Michael Howard <mikehow@microsoft.com>
2023-10-11 20:50:26 -0500

    Big update using Tokio

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::env;
-use std::net::TcpStream;
+use std::net::{SocketAddr, IpAddr};
+use tokio::net::TcpStream;
+use tokio::time::Duration;
 
 const COMMON_PORTS: [u16; 10] = [20, 21, 22, 23, 25, 80, 443, 587, 1433, 8080];
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
         println!("Usage: <program> <ip_range>");
@@ -11,24 +14,37 @@ fn main() {
         return;
     }
 
-    let base_ip = &args[1][..args[1].len() - 1]; // Remove the 'x' from the end
-    println!("Scanning on IP range: {}nnn:1-255", base_ip);
+    let base_ip = &args[1][..args[1].len() - 1];
+    println!("Scanning on IP range: {}[1-255]", base_ip);
+
+    let mut handles = vec![];
 
     for i in 1..255 {
-        let ip = format!("{}{}", base_ip, i);
-        scan_ip(&ip);
+        let ip = format!("{}{}", base_ip, i).parse().unwrap();
+        // Spawn a new asynchronous task for each IP address
+        let handle = tokio::spawn(scan_ip(ip));
+        handles.push(handle);
+    }
+
+    // Await all tasks to complete.
+    for handle in handles {
+        handle.await.unwrap();
     }
 }
 
-fn scan_ip(ip: &str) {
-    println!("\nScanning IP: {}", ip);
+async fn scan_ip(ip: IpAddr) {
 
     let mut open_ports = Vec::new();
 
     for &port in &COMMON_PORTS {
-        let ipaddr = format!("{}:{}", ip, port);
-        let socket  = ipaddr.parse().unwrap();
-        if TcpStream::connect_timeout(&socket, std::time::Duration::new(1, 0)).is_ok() {
+        let addr = SocketAddr::new(ip, port);
+        // Use tokio::time::timeout to add a timeout to the TcpStream::connect operation.
+        let result = tokio::time::timeout(
+            Duration::from_secs(1),
+            TcpStream::connect(&addr),
+        ).await;
+
+        if let Ok(Ok(_stream)) = result {
             open_ports.push(port);
         }
     }
@@ -40,7 +56,8 @@ fn scan_ip(ip: &str) {
         }
         println!();
     } else {
-        println!("No open ports found on {}.", ip);
+        //println!("No open ports found on {}.", ip);
     }
 }
+
 
